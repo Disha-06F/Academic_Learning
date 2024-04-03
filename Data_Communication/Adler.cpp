03fff036b084cd3a2cb1e026cf1f5c413daec8cc
H H Joti <150466181+Disha-06F@users.noreply.github.com>
2024-04-03 08:42:19 +0600

    Adler.cpp
    Adler-32 is a checksum algorithm written by Mark Adler in 1995
    
    modifying Fletcher's checksum. Compared to a cyclic redundancy check of the same length, it trades reliability for speed. Adler-32 is more reliable than Fletcher-16, and slightly less reliable than Fletcher-32.

---
+++ Data_Communication/Adler.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+uint32_t Adler_32(string data, int n)
+{
+uint32_t R = 0, L = 0, check_sum = 0;
+for (int i = 0; i < n; i++) {
+R = (R + data[i]) % 65521;
+L = (L + R) % 65521;
+}
+check_sum = L * 65536 + R;
+return check_sum;
+}
+int main()
+{
+string input = "BBH2101006F";
+cout <<"Adler's checksum code for the input in hexadecimal is: ";
+printf("%X\n", Adler_32(input, input.size()));
+}
